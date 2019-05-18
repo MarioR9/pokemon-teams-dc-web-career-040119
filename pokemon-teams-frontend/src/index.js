67a603aa789e06b9fa1689c4bff5eec0bd819cfd
Marior9 <mariorodriguezan@gmail.com>
2019-05-17 23:28:46 -0400

    DOM displays trainers and last pokemon

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,50 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetchTrainersData()
+})
+
+function fetchTrainersData(){
+fetch('http://localhost:3000/trainers')
+.then(response => response.json())
+.then(trainersData => trainersData.forEach(addTrainerToDom))    
+}
+
+//main will create more divs as need it
+
+function addTrainerToDom(trainer){
+    console.log
+    let main = document.querySelector("main")
+        let trainerDiv = document.createElement("div")
+            trainerDiv.className = "card"
+            trainerDiv.dataset.id = trainer.id        
+            let name = document.createElement("p")
+                let button = document.createElement("button")
+                    button.dataset.TrainerId = trainer.id  
+                    let ul = document.createElement("ul")
+                        let li = document.createElement('li')
+                            let liBtn = document.createElement('button')
+                                liBtn.className = 'release'
+                                
+                        
+                            
+
+            name.innerText = trainer.name
+            button.innerText = "catch a pokemon"
+            //iteration over pokemons to get name,species and assing pokemon id to each pokemon
+           trainer.pokemons.forEach(pokemon =>
+                            li.innerText = `${pokemon.nickname} (${pokemon.species})`,
+                            ul.appendChild(li),  
+                            liBtn.innerText = "Release")
+                                
+                                
+            main.append(trainerDiv)
+            trainerDiv.append(name,button,ul)
+            
+            li.appendChild(liBtn)
+
+
+
+    }   
